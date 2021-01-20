23d969a69bb0744268149b9ff7db85abd91a765a
magdychserg <75405768+magdychserg@users.noreply.github.com>
2021-01-20 21:56:55 +1200

    Update task2-2.py
    correct by range

--- task2-2.py
+++ task2-2.py
@@ -1,8 +1,6 @@
 #Замена введеных символов местами. 1 и 2 , 3 и 4 и т.д.
-j = 0
 spisok = list(input('Введите символы для создания списка: '))#создание списка
 print(f"Your list:", spisok)
-for i in range(int(len(spisok) / 2)):
-    spisok[j], spisok[j + 1] = spisok[j + 1], spisok[j]
-    j += 2
+for i in range(1, len(spisok), 2):
+    spisok[i-1], spisok[i] = spisok[i], spisok[i-1]
 print(f"New list:", spisok)
