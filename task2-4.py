e3d774f8ff86e8139c30425ffcf99fbdef9147d0
magdychserg <75405768+magdychserg@users.noreply.github.com>
2021-01-20 22:00:16 +1200

    Update task2-4.py
    correct for

--- task2-4.py
+++ task2-4.py
@@ -1,11 +1,4 @@
 # Выводим слова построчно если больше 10 выводим только 10
 words = str(input('Enter words separated by a space ')).split()
-a = 0
-for i in words:
-    a +=1
-    if len(i) > 10:
-        print(a, i[:10])
-    else:
-        print(a, i)
-
-
+for i, word in enumerate(words):
+    print(i, word[:10])
